422ce81b519dd0be13cfb0e1b8937cae7a3e5618
Ruben Schlaen <gschlaen@agro.uba.ar>
2024-05-08 17:03:58 -0300

    Initial Firebase setup with flutterfire + add firebase_core, firebase_auth packages, platform configuration on Android

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,16 @@
+import 'package:ecommerce_app/firebase_options.dart';
 import 'package:ecommerce_app/src/app_bootstrap.dart';
 import 'package:ecommerce_app/src/app_bootstrap_firebase.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 // ignore:depend_on_referenced_packages
 import 'package:flutter_web_plugins/url_strategy.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  // TODO: Initialize firebase
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   // turn off the # in the URLs on the web
   usePathUrlStrategy();
   // create an app bootstrap instance
